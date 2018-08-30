34fca22617ca4cf85b55d06c7504280ee98fca24
xuejingjing@gmail.com <xuejjgo@outlook.com>
2018-08-30 14:06:16 +0800

    add single runner

--- my_run/tran.py
+++ my_run/tran.py
@@ -5,9 +5,9 @@ import pandas as pd
 from scipy.sparse import coo_matrix
 import sys
 #import sh
-#import tensorflow as tf
+import tensorflow as tf
 
-#import wals
+import wals
 
 # ratio of train set size to test set size
 TEST_SET_RATIO = 10
@@ -69,7 +69,7 @@ def make_tran_and_test(input_file):
         ratings[:, 2] = np_ratings
     else:
         ratings = ratings_df.as_matrix(['user_id', 'item_id', 'rating'])
-        print(ratings)
+        #print(ratings)
         # deal with 1-based user indices
         ratings[:, 0] -= 1
         ratings[:, 1] -= 1
@@ -96,27 +96,80 @@ def _create_sparse_train_and_test(ratings, n_users, n_items):
     test_set_size = len(ratings) / TEST_SET_RATIO
     test_set_idx = np.random.choice(xrange(len(ratings)),
                                     size=test_set_size, replace=False)
-    print(test_set_size)
     test_set_idx = sorted(test_set_idx)
 
     # sift ratings into train and test sets
     ts_ratings = ratings[test_set_idx]
     tr_ratings = np.delete(ratings, test_set_idx, axis=0)
+    print(tr_ratings.shape)
 
     # create training and test matrices as coo_matrix's
     u_tr, i_tr, r_tr = zip(*tr_ratings)
     tr_sparse = coo_matrix((r_tr, (u_tr, i_tr)), shape=(n_users, n_items))
 
-    u_ts, i_ts, r_ts = zip(*ts_ratings)
+    u_ts, i_ts, r_ts = zip(*ts_ratings)    
     test_sparse = coo_matrix((r_ts, (u_ts, i_ts)), shape=(n_users, n_items))
 
     return tr_sparse, test_sparse
 
 
 
+def train_model(tr_sparse):
+    """Instantiate WALS model and use "simple_train" to factorize the matrix.
+
+    Args:
+        args: training args containing hyperparams
+        tr_sparse: sparse training matrix
+
+    Returns:
+        the row and column factors in numpy format.
+    """
+    dim = 5
+    num_iters = 20
+    reg = 0.07
+    unobs = 0.01
+    wt_type = 0
+    feature_wt_exp = 0.08
+    obs_wt = 130
+
+    tf.logging.info('Train Start: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+
+    # generate model
+    input_tensor, row_factor, col_factor, model = wals.wals_model(tr_sparse,
+                                                                    dim,
+                                                                    reg,
+                                                                    unobs,
+                                                                    True,
+                                                                    wt_type,
+                                                                    feature_wt_exp,
+                                                                    obs_wt)
+
+    # factorize matrix
+    session = wals.simple_train(model, input_tensor, num_iters)
+
+    print('end')
+    tf.logging.info('Train Finish: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+
+    # evaluate output factor matrices
+    output_row = row_factor.eval(session=session)
+    output_col = col_factor.eval(session=session)
+
+    # close the training session now that we've evaluated the output
+    session.close()
+
+    return output_row, output_col
+
+
 
 def main():
-    make_tran_and_test(sys.argv[1])
+    user_map, item_map, tr_sparse, test_sparse = make_tran_and_test(sys.argv[1])
 
+    output_row, output_col = train_model(tr_sparse)
+
+    train_rmse = wals.get_rmse(output_row, output_col, tr_sparse)
+    test_rmse = wals.get_rmse(output_row, output_col, test_sparse)
+
+    print(train_rmse)
+    print(test_rmse)
 
 main()
